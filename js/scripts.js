b21ca954882a04bd88c00e186274e871536165af
Emily Mackay <mackaye@protonmail.com>
2020-08-22 22:43:27 -0700

    Adds sticky bar to mobile first layout

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,11 @@ $(document).ready(function() {
   $(window).scroll(function() {
     if ($(document).scrollTop() > 470) {
       $("nav").removeClass("remove-colour-bg").addClass("add-colour-bg");
+      $("#menu ul").removeClass("remove-mobile-menu-nav-ul").addClass("add-mobile-menu-nav-ul");
     }
     else {
       $("nav").removeClass("add-colour-bg").addClass("remove-colour-bg");
+      $("#menu ul").removeClass("add-mobile-menu-nav-ul").addClass("remove-mobile-menu-nav-ul");
     }
   });
 
